7d87116546afaa55424489c337a84540e147d6c9
Shreya Sanap <sanapshreya2003@gmail.com>
2023-07-14 15:59:18 +0530

    fix bugs

--- app.py
+++ app.py
@@ -5,28 +5,31 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        short_name = request.form.get('name')
-        url = request.form.get('url')
-        if 'http' not in url:
-            url = f'http://{url}'
-        if valid_url(url):
-            if name_available(short_name) is None:
-                add_url(short_name, url)
-                return render_template('success.html', short_name=short_name)
-            else:
-                return render_template('index.html', msg='Short name not available')
-        else:
-            return render_template('index.html', msg='Invalid URL')
+  if request.method == "POST":
+    short_name = request.form.get('name')
+    url = request.form.get('url')
 
-    return render_template('index.html')
+    if 'http' not in url:
+      url = f'http://{url}'
+    
+    if valid_url(url):
+      if name_available(short_name) is None:
+        add_url(short_name, url)
+        return render_template("success.html", short_name=short_name)
+      else:
+        return render_template('index.html', msg='Short name not available')
+    else:
+      return render_template('index.html', msg='Invalid url')
 
-@app.route('/<path:path_name>')
-def redirect_url(path_name):
-    url = name_available(path_name)
-    if url is None:
-        return "<h2 style='color:red'>Invalid URL</h2>"
-    return redirect(url)
+  return render_template('index.html')
+
+
+@app.route('/<path:short_name>')
+def redirect_url(short_name):
+  url = get_url(short_name)
+  if url is None:
+    return "<h2 style='color:red'> Invalid URL </h2>"
+  return redirect(url[2])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
